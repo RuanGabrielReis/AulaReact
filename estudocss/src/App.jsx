d4c1b58073bb5b0039e60b00e8012b2ec3af41b1
Ruan <ruangabreis28@gmail.com>
2025-06-05 11:05:21 -0300

    05/06

--- estudocss/src/App.jsx
+++ estudocss/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
     <div>
       <h1>Estudo do CSS no React</h1>
 
+      <Exemplo1/>
+      <hr/>
       <Exemplo2/>
     </div>
   )
--- estudocss/src/paginas/Exercicio2.jsx
+++ estudocss/src/paginas/Exercicio2.jsx
@@ -1,9 +1,9 @@
 export default function Exemplo2(){
     return (
         <div>
-            <h2 style={{color: "red"}}>Exemplos de Estilos em Linha</h2>
+            <h2 style={{color: "red",backgroundColor: "yellow",textAlign: "center", padding: "10px"}}>Exemplos de Estilos em Linha</h2>
 
-            <p>I am thou. Thou art I. Thou has established a new bond. Thou shalt have our blessings when thou chooseth to create a Persona of the X arcana.  </p>
+            <p>I am thou. Thou art I. Thou has established a <span style={{color: "purple", fontStyle: "italic"}}>new bond</span>. Thou shalt have our blessings when thou chooseth to create a Persona of the X arcana.  </p>
         </div>
     )
 } 
