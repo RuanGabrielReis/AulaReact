c36b433cb1dc2e96c812568f20dfadc1e3cd62ea
RuanGabrielReis <ruangabreis28@gmail.com>
2025-08-28 08:59:47 -0300

    28/08

--- aula6/src/Paginas/Exemplo1.jsx
+++ aula6/src/Paginas/Exemplo1.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {useState} from "react";
 
 export default function Exemplo1() {
+  const [nome, setNome] = useState("Ruan");
+  const [idade, setIdade] = useState(18);
+  const [resultado, setResultado] = useState(null);
+
+  function calcular() {
+
+    let dias = Number(idade) * 365;
+    setResultado("Olá "+ nome + ", você já viveu " + dias + " dias.");
+  }
+
+  function limpar() {
+    setNome("");
+    setIdade(0);
+    setResultado(null);
+  }
+
   return (
     <div>
+      
       <h1>Exemplo 1</h1>
+      <div className="container">
+        <form>
+          <p>
+            Digite o nome do aluno <br/>
+            <input type="text" placeholder="Digite seu nome" value={nome} onChange={e => setNome(e.target.value)}/>
+          </p>
+          <p>
+            Digite a idade do aluno <br/>
+            <input type="number" placeholder="Digite sua idade" value={idade} onChange={e => setIdade(e.target.value)}/>
+          </p>
+          <p style={{ textAlign: "center" }}>
+            <input type="button" value="Exemplo 1" onClick={calcular}/>
+            <input type="button" value="Limpar" onClick={limpar}/>  
+          </p>
+
+          <p>
+            <b>Resultado:</b> <br/>
+            {resultado}
+          </p>
+
+          <p style={{ textAlign: "right" }}>
+            <Link to="/">
+              <input type="button" title="Voltar para Home" value="&#8592;" style={{ width: "auto" }}/>
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
--- aula6/src/Paginas/Exemplo2.jsx
+++ aula6/src/Paginas/Exemplo2.jsx
@@ -1,9 +1,84 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {useState} from "react";
 
 export default function Exemplo2() {
+  const [n1, setN1] = useState(0);
+  const [n2, setN2] = useState(0);
+  const [resultado, setResultado] = useState(null);
+  const [operacao, setOperacao] = useState("soma");
+
+  function calcular() {
+
+    let soma = Number(n1) + Number(n2);
+    let subtracao = Number(n1) - Number(n2);
+    let multiplicacao = Number(n1) * Number(n2);
+    let divisao = Number(n1) / Number(n2);
+
+    switch (operacao) {
+      case "soma":
+        setResultado("A soma de " + n1 + " e " + n2 + " é " + soma + ".");
+        break;
+      case "subtracao":
+        setResultado("A subtração de " + n1 + " e " + n2 + " é " + subtracao + ".");
+        break;
+      case "multiplicacao":
+        setResultado("A multiplicação de " + n1 + " e " + n2 + " é " + multiplicacao + ".");
+        break;
+      case "divisao":
+        setResultado("A divisão de " + n1 + " e " + n2 + " é " + divisao + ".");
+        break;
+      default:
+        setResultado("Operação inválida.");
+    }
+  }
+
+  function limpar() {
+    setN1(0);
+    setN2(0);
+    setResultado(null);
+  }
+
   return (
     <div>
+      
       <h1>Exemplo 2</h1>
+      <div className="container">
+        <form>
+          <p>
+            Digite o número 1 <br/>
+            <input type="number" placeholder="Digite o número 1" value={n1} onChange={e => setN1(e.target.value)}/>
+          </p>
+          <p>
+            Digite o número 2 <br/>
+            <input type="number" placeholder="Digite o número 2" value={n2} onChange={e => setN2(e.target.value)}/>
+          </p>
+          <p>
+            Escolha a operação <br/>
+            <select onChange={e => setOperacao(e.target.value)} value={operacao}>
+              <option value="soma">Soma</option>
+              <option value="subtracao">Subtração</option>
+              <option value="multiplicacao">Multiplicação</option>
+              <option value="divisao">Divisão</option>
+            </select>
+          </p>
+          <p style={{ textAlign: "center" }}>
+            <input type="button" value="Calcular" onClick={calcular}/>
+            <input type="button" value="Limpar" onClick={limpar}/>  
+          </p>
+
+          <p>
+            <b>Resultado:</b> <br/>
+            {resultado}
+          </p>
+
+          <p style={{ textAlign: "right" }}>
+            <Link to="/">
+              <input type="button" title="Voltar para Home" value="&#8592;" style={{ width: "auto" }}/>
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
