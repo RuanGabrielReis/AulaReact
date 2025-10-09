2f122455d27d7e481493fbf36c629fe506de7fdb
RuanGabrielReis <ruangabreis28@gmail.com>
2025-10-09 09:02:26 -0300

    09/10

--- aulaBD/servidor/index.js
+++ aulaBD/servidor/index.js
@@ -11,6 +11,37 @@ const banco = mysql2.createConnection({
     host : "localhost",
     port : 3308,
     user : "root",
-    passowrd : "mysqlfatec",
+    password : "mysqlfatec",
     database : "aulabd"
+});
+
+banco.connect((erro) => {
+    if(erro) {
+        console.log("Erro ao conectar ao MySQL: ");
+        console.log(erro);
+    } else {
+        console.log("Conectado ao MySQL com sucesso!");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando em https://localhost:"+PORT);
+});
+
+app.get("/alunos", (req,res) => {
+    const sql = "SELECT * FROM alunos";
+
+    banco.query(aql,(erro,resultados) => {
+        if (erro) {
+            console.log(erro);
+            return res.status(500).json({error: "Erro ao consultar alunos"});
+        } else {
+            console.log(resultados);
+            return res.status(200).json(resultados);
+        }
+    });
+});
+
+app.get("/alunos/:codigo", (req,res) => {
+
 });
